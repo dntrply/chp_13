24331dd0404994de80a32f05796c2411d12f0780
dntrply <dntrplytch@gmail.com>
2019-06-01 20:30:26 -0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class unitConverterTile extends StatelessWidget {
         child: InkWell(
           highlightColor: Colors.purple,
           onTap: () {
-            print('I was tpped');
+            print('I was tapped');
           },
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
